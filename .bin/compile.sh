9466319b1843ebcdcfeb66867f614fa040a011f5
Adam <akoeh001@cs.ucr.edu>
2014-03-26 02:07:43 -0400

    reconfigure to ignore errant output and properly optimize for single file but still also do multiple files

--- .bin/compile.sh
+++ .bin/compile.sh
@@ -9,22 +9,40 @@ if [ "${errors}" != "" ]
 then
     if [ "${UCRCS_COURSE}" == "CS010" -o "${UCRCS_COURSE}" == "CS010v" ]
     then
-        for var in "$@"
-        do
-            cp $var tmp.cpp
-            myerrs=$(g++ tmp.cpp 2>&1)
-            sed -i "1,/END ASS/d" tmp.cpp
-            value=$(cat tmp.cpp)        
-            rm tmp.cpp
+        if [ "$#" -gt 1 ]; then
+            mkdir cmp_tmp &>/dev/null
+            for var in "$@"
+            do
+                myerrs=$(g++ -c $var -o cmp_tmp/$var.o 2>&1)
+                cp $var _no_name_tmp.cpp &>/dev/null
+                sed -i "1,/END ASS/d" _no_name_tmp.cpp &>/dev/null
+                value=$(cat _no_name_tmp.cpp 2>/dev/null)        
+                rm _no_name_tmp.cpp &>/dev/null
+                if [ "$value" == "" ]
+                then
+                    value=$(cat $1 2>/dev/null)
+                fi
+                code_val="$(perl -MURI::Escape -e 'print uri_escape($ARGV[0]);' "$value")"
+                err_val="$(perl -MURI::Escape -e 'print uri_escape($ARGV[0]);' "$myerrs")"
+                rsub_val="$(perl -MURI::Escape -e 'print uri_escape($ARGV[0]);' "$UCRCS_UCRSUB_EMAIL")"        
+                curl "${FORM_LOCATION}?${CODE_AREA}=${code_val}&${ERROR_AREA}=${err_val}&${RSUB_EMAIL_AREA}=${rsub_val}" &>/dev/null
+            done
+            rm -rf cmp_tmp &>/dev/null
+        elif [ "$#" -gt 0 ]; then
+            cp $var _no_name_tmp.cpp &>/dev/null
+            sed -i "1,/END ASS/d" _no_name_tmp.cpp &>/dev/null
+            value=$(cat _no_name_tmp.cpp 2>/dev/null)        
+            rm _no_name_tmp.cpp &>/dev/null
             if [ "$value" == "" ]
             then
-                value=$(cat $1)
+                value=$(cat $1 2>/dev/null)
             fi
             code_val="$(perl -MURI::Escape -e 'print uri_escape($ARGV[0]);' "$value")"
-            err_val="$(perl -MURI::Escape -e 'print uri_escape($ARGV[0]);' "$myerrs")"
+            err_val="$(perl -MURI::Escape -e 'print uri_escape($ARGV[0]);' "$errors")"
             rsub_val="$(perl -MURI::Escape -e 'print uri_escape($ARGV[0]);' "$UCRCS_UCRSUB_EMAIL")"        
-            curl "${FORM_LOCATION}?${CODE_AREA}=${code_val}&${ERROR_AREA}=${err_val}&${RSUB_EMAIL_AREA}=${rsub_val}" &>/dev/null
-        done
+            curl "${FORM_LOCATION}?${CODE_AREA}=${code_val}&${ERROR_AREA}=${err_val}&${RSUB_EMAIL_AREA}=${rsub_val}" &>/dev/null        
+        fi
     fi
     echo "$errors"
 fi
+
